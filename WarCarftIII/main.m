ee97495cd41a2208b2a80a8cf48e0e6a95ed9263
Aaron Dufall <aald212@gmail.com>
2014-05-05 13:12:55 -0700

    Initial Commit

---
+++ WarCarftIII/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WarCarftIII
+//
+//  Created by Aaron Dufall on 5/05/2014.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
