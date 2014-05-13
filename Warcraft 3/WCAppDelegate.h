2f242e2e9e89e7fe86320cd2722b11ca3a619d89
matrinox <geofflee21@me.com>
2014-05-12 17:02:25 -0700

    Removed Geoff from copyright

--- Warcraft 3/WCAppDelegate.h
+++ Warcraft 3/WCAppDelegate.h
@@ -2,7 +2,6 @@
 //  WCAppDelegate.h
 //  Warcraft 3
 //
-//  Created by Geoff on 2014-05-09.
 //  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
 //
 
--- Warcraft 3/WCAppDelegate.m
+++ Warcraft 3/WCAppDelegate.m
@@ -2,7 +2,6 @@
 //  WCAppDelegate.m
 //  Warcraft 3
 //
-//  Created by Geoff on 2014-05-09.
 //  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
 //
 
