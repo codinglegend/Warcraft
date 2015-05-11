bfc494295aee5cc19c561dfd6e2a559d2f677b7a
Cory Alder <coryalder@gmail.com>
2015-05-11 16:54:06 -0700

    Removed reference to .rb from warcraft assignment
    No need to make a Unit class, it already exists in the repo they're forking.

--- Warcraft 3Tests/WarCraftIII_Tests_05.m
+++ Warcraft 3Tests/WarCraftIII_Tests_05.m
@@ -12,7 +12,7 @@
 
 // A unit is a common term in strategy games like Warcraft 3 and it stands for a soldier or person that performs a task of some kind. A footman is just one of many "units" that a barracks can train.
 
-//So let's use inheritence. First we'll introduce a new class (create a new file called unit.rb for it).
+//So let's use inheritence. First we'll introduce a new class called `Unit`.
 // The Unit is an abstract idea and thus it needs to be told what the HP and AP for the unit will be
 
 // Since all units have healthPoints, the HP related stuff can be migrated/moved into the Unit class instead.
